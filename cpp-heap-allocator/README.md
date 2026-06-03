da863c9806322a95beb09a83f109382d5c99bf5f
rb152080 <rb747@protonmail.com>
2026-06-03 18:04:28 -0400

    finished cpp-heap-allocator code

---
+++ cpp-heap-allocator/README.md
@@ -0,0 +1,9 @@
+# C++ Custom Heap Allocator
+
+mimics how malloc would work
+inspired by [salar's video](https://www.youtube.com/watch?v=H8vCBFX62Qg)
+
+![split_available_block().png](./excalidraw/split_available_block().png)
+
+![block.png](./excalidraw/block.png)
+
--- cpp-heap-allocator/main.cpp
+++ cpp-heap-allocator/main.cpp
@@ -1,9 +1,11 @@
 #include <array>
 #include <cstddef>
 #include <iostream>
+#include <print>
 
 // evaluated at compile time instead of run time
-constexpr std::size_t HEAP_SIZE{1024 * 1024};
+// this is the actual heap
+constexpr std::size_t HEAP_SIZE{1024 * 1024}; // 1 mb/1048576 bytes
 
 // aligning in memory so that all data types can be aligned properly; starts at
 // a memory address that is a multiple of std::max_align_t
@@ -23,6 +25,7 @@ void initialize_head_of_heap() {
   // you less freedom; reinterpret_cast trusts you; .data() only callable on
   // data that is contiguous
   head = reinterpret_cast<Block *>(heap.data());
+  head->available_ = true; // make sure to do this when doing reinterpret_cast
   head->size_ = HEAP_SIZE - sizeof(Block);
   head->next_ = nullptr;
 }
@@ -42,7 +45,7 @@ void split_available_block(Block *block, size_t size) {
   // the struct Block will store available_, size_, next_
   // we cast it to a std::byte* first since its multiple of 1, then we can do
   // the (block + 1) + size calculation properly
-  auto *next_block{reinterpret_cast<Block *>(
+  Block *next_block{reinterpret_cast<Block *>(
       reinterpret_cast<std::byte *>(block + 1) + size)};
   next_block->size_ = block->size_ - sizeof(Block) - size;
   // you have to set this to true manually since reinterpret_cast just treats it
@@ -56,7 +59,7 @@ void split_available_block(Block *block, size_t size) {
   block->next_ = next_block;
 }
 
-void *allocate_memory(size_t size) {
+void *allocate(size_t size) {
   if (size == 0) { // if no memory needs to be allocated
     return nullptr;
   }
@@ -65,7 +68,7 @@ void *allocate_memory(size_t size) {
   }
   size = align_up(size, alignof(std::max_align_t)); // alignof (c++11)
 
-  auto *current{head}; // current is a Block ptr
+  Block *current{head}; // current is a Block ptr
 
   while (current) {
     if (current->available_ && current->size_ >= size) {
@@ -76,15 +79,63 @@ void *allocate_memory(size_t size) {
       // header: Block { available_, size_, next_ }
       // payload: where the actual data will go
       return current + 1; // returns where the payload starts
+      // it does implicit pointer conversion; currently it is a Block*
+      // return (void *)(current + 1); // explicit C-style pointer conversion
+      // return static_cast<void *>(current + 1); // explicit C++ style pointer
+      // conversion
     }
     current = current->next_;
   }
   return nullptr;
 }
 
-void deallocate_memory(void *pointer) {}
+// [available] [available] [available] -> [available]
+void merge_available_blocks() {
+  Block *current{head};
+  while (current && current->next_) {
+    if (current->available_ && current->next_->available_) {
+      current->size_ += sizeof(Block) + current->next_->size_;
+      current->next_ = current->next_->next_;
+    } else {
+      current = current->next_;
+    }
+  }
+}
+
+void deallocate(void *pointer) {
+  if (!pointer) {
+    return;
+  }
+  Block *block{reinterpret_cast<Block *>(pointer) - 1};
+  block->available_ = true;
+  merge_available_blocks();
+}
+
+void print_heap() {
+  size_t index{};
+  Block *current{head};
+  std::cout << "printing heap...\n";
+  while (current) {
+    std::println("block {}: {}, {} bytes", ++index,
+                 current->available_ ? "available" : "unavailable",
+                 current->size_);
+    current = current->next_;
+  }
+  std::cout << "finished printing heap\n";
+}
 
 int main() {
-  std::cout << sizeof(Block) << "\n";
+  // std::cout << sizeof(Block) << "\n";
+  print_heap();
+  void *ptr1{allocate(10)};
+  void *ptr2{allocate(20)};
+  print_heap(); // block 1: unavailable, 34 bytes
+                // block 2: unavailable, 44 bytes
+                // block 3: available, 1 mb - 30 bytes
+  deallocate(ptr1);
+  deallocate(ptr2);
+  print_heap();
+  void *ptr3{allocate(30)};
+  print_heap();
   return 0;
 }
