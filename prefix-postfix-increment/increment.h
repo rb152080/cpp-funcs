af99500a5fa93326fd3bbd1e37b49f790b0fae43
rb152080 <rb747@protonmail.com>
2026-08-17 21:03:33 -0400

    implemented prefix, postfix increment overrides

---
+++ prefix-postfix-increment/increment.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+class Increment
+{
+  private:
+    int value_;
+    int increment_;
+
+  public:
+    Increment(int value = 0, int increment = 1)
+        : value_(value), increment_(increment)
+    {}
+
+    ~Increment() {}
+
+    Increment& operator++()
+    {
+        value_ += increment_;
+        return *this;
+    }
+
+    Increment operator++(int)
+    {
+        Increment temp = *this;
+        value_ += increment_;
+        return temp;
+    }
+
+    void print() const
+    {
+        std::cout << value_ << '\n';
+    }
+};
---
+++ prefix-postfix-increment/main.cpp
@@ -0,0 +1,8 @@
+#include "increment.h"
+
+int main(void)
+{
+    Increment i; // default is 0
+    (i++).print();
+    return 0;
+}
